6f3450ea2d8ff0a15cf8843617097d1a084a2b36
Anna-Smith <ann_v_smith@bk.ru>
2016-03-03 02:20:49 +0500

    * доп задания

--- multivarka.js
+++ multivarka.js
@@ -10,10 +10,12 @@ module.exports = {
     },
     collection: function (colName) {
         this.colName = colName;
+        this.notFlag = false;
         return this;
     },
     where: function (field) {
         this.field = field;
+        this.notFlag = false;
         return this;
     },
     equal: function (value) {
@@ -36,26 +38,71 @@ module.exports = {
         this.notFlag = !this.notFlag;
         return this;
     },
+    set: function (field, value) {
+        let updateData = {};
+        updateData[field] = value;
+        this.updateExpr = {$set: updateData};
+        return this;
+    },
     find: function (cb) {
-        mongoClient.connect(this.url, (err, db) => {
-            const col = db.collection(this.colName);
-            let condition = getCondition(this);
-            col.find(condition).toArray(function (err, result) {
-                cb(null, result);
-                db.close();
-            });
-        });
+        const thisCopy = Object.assign({}, this);
+        doMongoRequest(thisCopy, 'find', cb);
+    },
+    remove: function (cb) {
+        const thisCopy = Object.assign({}, this);
+        doMongoRequest(thisCopy, 'remove', cb);
+    },
+    update: function (cb) {
+        const thisCopy = Object.assign({}, this);
+        doMongoRequest(thisCopy, 'update', cb);
+    },
+    insert: function (obj, cb) {
+        this.obj = obj;
+        const thisCopy = Object.assign({}, this);
+        doMongoRequest(thisCopy, 'insert', cb);
     }
 };
 
 function getCondition(thisArg) {
     let condition = {};
-    if (thisArg.notFlag) {
-        let expression = {};
-        expression.$not = thisArg.expression;
-        condition[thisArg.field] = expression;
-    } else {
-        condition[thisArg.field] = thisArg.expression;
+    if (thisArg.field && thisArg.expression) {
+        if (thisArg.notFlag) {
+            let expression = {};
+            expression.$not = thisArg.expression;
+            condition[thisArg.field] = expression;
+        } else {
+            condition[thisArg.field] = thisArg.expression;
+        }
     }
     return condition;
+}
+
+function doMongoRequest(multivarka, type, cb) {
+    mongoClient.connect(multivarka.url, (err, db) => {
+        const col = db.collection(multivarka.colName);
+        let condition = getCondition(multivarka);
+        switch (type) {
+            case 'find':
+                col.find(condition).toArray(callback);
+                break;
+            case 'remove':
+                col.deleteMany(condition, callback);
+                break;
+            case 'update':
+                col.updateMany(condition, multivarka.updateExpr, callback);
+                break;
+            case 'insert':
+                if (multivarka.obj instanceof Array) {
+                    col.insertMany(multivarka.obj, callback);
+                } else {
+                    col.insertOne(multivarka.obj, callback);
+                }
+                break;
+        }
+
+        function callback(err, result) {
+            cb(err, result);
+            db.close();
+        }
+    });
 }
